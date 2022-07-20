564398b87dee1a8a2046b7a5725e6909732fa1bd
Jeff C Sprague <jeffcsprague@gmail.com>
2022-07-19 20:31:17 -0400

    dayjs

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,5 +2,8 @@
 const User = require("./User.js")
 const Day = require("./Day.js")
 const Review = require("./Review.js")
+const dayjs = require('dayjs')
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format()
 
-module.exports = {User, Day, Review};
+module.exports = {User, Day, Review, dayjs};
