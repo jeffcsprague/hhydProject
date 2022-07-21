06caa8c372e382fbd918d238589a059c2cdf5e37
jeffcsprague <102316153+jeffcsprague@users.noreply.github.com>
2022-07-20 22:37:51 -0400

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const Home = (props) => {
+    return (
+        <div>
+            <div className="grid-x align-center home-page">RATE YOUR DAY
+            <div className="cell small-12 align-center text-center logo">
+            </div>
+            </div>
+            <div className="grid-x align-center create-account-button">
+                <Link to="/users/new" className="button expanded" href="#">Create an Account</Link>
+            </div>
+            <div className="grid-x align-center align-middle footer">© 2022 Hey How’s Your Day
+            </div>
+        </div>
+    )
+}
+
+export default Home
